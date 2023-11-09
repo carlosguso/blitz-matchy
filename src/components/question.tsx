4f68e8c9f2322bd9396111450bf9245f568ad103
Carlos Gutierrez Solis <carlosguso@MacBook-Air-de-Carlos.local>
2023-11-08 19:00:22 -0600

    Remove Next's Image component

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -39,13 +39,10 @@ export default function Question(props: Question) {
     return (
     <>
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-            <Image
+            <img
             src={props.image}
             alt={`Image highly related to the question: ${props.question}`}
-            width={180}
-            height={37}
-            priority
-            className='border-2 border-red-700'
+            className='border-2 border-red-700 w-40 h-auto'
             />
         </div>
         <h3 className='mt-2'>
