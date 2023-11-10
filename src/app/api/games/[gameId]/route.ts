521c3971507518522103b2bbeabc99727e9d525c
Carlos Gutierrez Solis <carlosguso@MacBook-Air-de-Carlos.local>
2023-11-09 21:16:21 -0600

    Forgot to save

--- src/app/api/games/[gameId]/route.ts
+++ src/app/api/games/[gameId]/route.ts
@@ -7,7 +7,8 @@ export async function GET(
     try {
       const gameId = params.gameId
       connectDB()
-      return Response.json({"val": gameId})
+      const game = await Games.findById(gameId)
+      return Response.json(game)
     } catch (e) {
       return Response.json({"error": String(e)}, {status: 500})
     }
