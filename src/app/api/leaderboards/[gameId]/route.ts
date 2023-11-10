2a3e395b140256da92e17c77ba8cfb5b70d7abc8
Carlos Gutierrez Solis <carlosguso@MacBook-Air-de-Carlos.local>
2023-11-09 22:06:28 -0600

    Add new scores feature

--- src/app/api/leaderboards/[gameId]/route.ts
+++ src/app/api/leaderboards/[gameId]/route.ts
@@ -14,3 +14,38 @@ export async function GET(
       return Response.json({"error": String(e)}, {status: 500})
     }
   }
+
+export async function POST(
+  request: Request,
+  { params }: { params: { gameId: string } }
+) {
+  try {
+    const gameId = params.gameId
+    const body: {
+      user: string;
+      bestScoreSeconds: number;
+      bestScorePoints: number;
+    } = await request.json();
+
+    connectDB();
+    const userScore = await LeaderBoard.findOne({ gameId, user: body.user })
+    if (userScore) {
+      if (body.bestScorePoints > userScore.bestScorePoints) {
+        userScore.bestScorePoints = body.bestScorePoints;
+      }
+
+      if (body.bestScoreSeconds > userScore.bestScoreSeconds){
+        userScore.bestScoreSeconds = body.bestScoreSeconds;
+      }
+
+      await userScore.save()
+      return Response.json({userScore})
+    }
+    
+    const newUserScore = new LeaderBoard({...body, gameId})
+    await newUserScore.save()
+    return Response.json(newUserScore)
+  } catch (e) {
+    return Response.json({"error": String(e)}, {status: 500})
+  }
+}
