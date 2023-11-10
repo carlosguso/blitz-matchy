a07be3d5c8b265417523a4cddde537daffe71afc
Carlos Gutierrez Solis <carlosguso@MacBook-Air-de-Carlos.local>
2023-11-09 22:39:54 -0600

    t

--- src/app/api/games/[gameId]/route.ts
+++ src/app/api/games/[gameId]/route.ts
@@ -5,6 +5,7 @@ export async function GET(
     { params }: { params: { gameId: string } }
   ) {
     try {
+      console.log('Hi logs')
       const gameId = params.gameId
       connectDB()
       const game = await Games.findById(gameId)
